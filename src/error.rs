808dba7bab6c50e03115076de60e63c27c92e2f3
Dolpheyn <farissufyan99@gmail.com>
2021-11-23 09:13:27 +0800

    error: Add EscrowError::NotRentExempt, and impl From<EscrowError> for ProgramError

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,20 @@
 use thiserror::Error;
 
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Clone, Copy)]
 pub enum EscrowError {
     /// Invalid instruction
-    #[error("Invalid InvalidInstruction")]
+    #[error("Invalid instruction")]
     InvalidInstruction,
+
+    /// Lamport balance below rent-exempt threshold.
+    #[error("Lamport balance below rent-exempt threshold")]
+    NotRentExempt,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
 }
