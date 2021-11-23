9f754bb1105cfcc67beb2aed3ebc0859478dd2a3
Dolpheyn <farissufyan99@gmail.com>
2021-11-23 09:16:35 +0800

    {instruction, processor}: implement escrow initialization

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,5 +1,6 @@
-use solana_program::program_error::ProgramError;
 use crate::error::EscrowError::InvalidInstruction;
+use solana_program::program_error::ProgramError;
+use std::convert::TryInto;
 
 pub enum EscrowInstruction {
     /// Starts the trade by creating and populating an escrow account and transferring ownership of
@@ -20,13 +21,30 @@ pub enum EscrowInstruction {
     InitEscrow {
         /// The amount party A expects to receive of token Y.
         amount: u64,
-    }
+    },
 }
 
 impl EscrowInstruction {
+    /// The entrypoint of `instruction`.
     /// Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        let (tag, rest) = input.split_first().ok_or(InvalidInstruction);
-        Ok
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
+    }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        // Get the first 8 bytes (because u64 is 8 bytes long) and cast it into u64.
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
     }
 }
---
+++ src/processor.rs
@@ -0,0 +1,71 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    rent::Rent,
+    sysvar::Sysvar,
+    program_pack::{Pack, IsInitialized},
+};
+
+use crate::{state::Escrow, error::EscrowError, instruction::EscrowInstruction};
+
+pub struct Processor;
+
+impl Processor {
+    /// The entrypoint of `processor`.
+    pub fn process(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let instruction = EscrowInstruction::unpack(instruction_data)?;
+
+        match instruction {
+            EscrowInstruction::InitEscrow { amount } => {
+                msg!("Instruction: InitEscrow");
+                Self::process_init_escrow(accounts, amount, program_id)
+            }
+        }
+    }
+
+    pub fn process_init_escrow(
+        accounts: &[AccountInfo],
+        amount: u64,
+        program_id: &Pubkey,
+    ) -> ProgramResult {
+        // Get the first account, which should be the signer.
+        let account_info_iter = &mut accounts.iter();
+        let initializer = next_account_info(account_info_iter)?;
+
+        if !initializer.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+
+        let temp_token_account = next_account_info(account_info_iter)?;
+        let token_receiver_account = next_account_info(account_info_iter)?;
+
+        // Check: the token receiver account should be owned by the token program.
+        if *token_receiver_account.owner != spl_token::id() {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        let escrow_account = next_account_info(account_info_iter)?;
+        let rent = &Rent::from_account_info(next_account_info(account_info_iter)?)?;
+
+        // Check: Make sure the escrow account is exempted from rent.
+        if !rent.is_exempt(escrow_account.lamports(), escrow_account.data_len()) {
+            return Err(EscrowError::NotRentExempt.into());
+        }
+
+        let mut escrow_info = Escrow::unpack_unchecked(&escrow_account.try_borrow_data()?)?;
+
+        // Check: Make sure the escrow has not been initialized before.
+        if escrow_info.is_initialized() {
+            return Err(ProgramError::AccountAlreadyInitialized);
+        }
+
+        Ok(())
+    }
+}
